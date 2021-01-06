b81cf40253acc52d578f82cec6d78b7452b14508
Parveen Kumar <parveen.kumar6@gmail.com>
2021-01-07 01:46:51 +0530

    70

--- cpp/70ClimbingStairs.cpp
+++ cpp/70ClimbingStairs.cpp
@@ -5,19 +5,18 @@ using namespace std;
 class Solution{
     public:
         int climbStairs(int n){
+            if(n == 1) return 1;
             vector<int> steps(n);
             steps[0] = 1;
             steps[1] = 2;
-            for(int i = 2; i <= steps.size(); i++){
-                cout << "executed " << "\n";
-                steps[i] = steps[i-1] + steps[i - 2];
+            for(int i = 2; i <=(int) steps.size(); i++){
+              steps[i] = steps[i-1] + steps[i - 2];
             }
             return steps[n - 1];
         }
 };
 
 int main(){
-    int n = 3;
     Solution s;
     int out = s.climbStairs(3);
     cout << "Output : " << out << endl;
